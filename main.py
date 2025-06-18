4ccc7c30349a02b61c71e4be8f319728777785a6
salocin-95 <renicolas7@gmail>
2025-06-18 17:32:57 -0300

    Cambia algunas partes del código principal y de util.py

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
 import time
 from util import *
 
+# Llama al algoritmo que deseamos utilizar
 def llamar_algoritmo(algoritmo, lista, nombre_algoritmo, nombres_ordenados):
-    
-    if algoritmo == quicksort:
-        start = time.perf_counter()
-        algoritmo(lista, 0,len(lista) - 1)
-        end = time.perf_counter()
-        duration = end-start
-        write(lista, nombres_ordenados, "Apellido", "Nombre")
-        print(f"El algoritmo {nombre_algoritmo} tarda {duration:.6f}s para la cantidad de datos de la lista de {len(lista)}")
-    else:
         start = time.perf_counter()
         algoritmo(lista)
         end = time.perf_counter()
         duration = end-start
         write(lista, nombres_ordenados, "Apellido", "Nombre")
-        print(f"El algoritmo {nombre_algoritmo} tarda {duration:.6f}s para la cantidad de datos de la lista de {len(lista)}")
+        print(f"El algoritmo {nombre_algoritmo} tarda {duration:.6f}s para la cantidad de datos de la lista de {len(lista)} datos")
 
 def main():
     
     # Crea un array a partir de los archivos .csv
-
     array_10 = format("nombres-10.csv", "Apellido", "Nombre")
     array_100 = format("nombres-100.csv", "Apellido", "Nombre")
     array_1000 = format("nombres-1000.csv", "Apellido", "Nombre")
     array_10000 = format("nombres-10000.csv", "Apellido", "Nombre")
     
-    user_input = input("Elegí el algoritmo que queres usar:\n1. Bubble Sort\n2. Merge Sort\n3. QuickSort\n")
+    user_input = input("Elegí el algoritmo que queres usar:\n1. Bubble Sort\n2. Merge Sort\n")
     
     if user_input == "1":
         llamar_algoritmo(bubble_sort, array_10, "Bubble Sort", "nombres-10-ordenados.csv")
@@ -41,14 +32,8 @@ def main():
         llamar_algoritmo(merge_sort, array_1000, "Merge Sort","nombres-1000-ordenados.csv")
         llamar_algoritmo(merge_sort, array_10000, "Merge Sort","nombres-10000-ordenados.csv")
         pass
-    elif user_input == "3":
-        llamar_algoritmo(quicksort, array_10, "QuickSort","nombres-10-ordenados.csv")
-        llamar_algoritmo(quicksort, array_100, "QuickSort","nombres-100-ordenados.csv")
-        llamar_algoritmo(quicksort, array_1000, "QuickSort","nombres-1000-ordenados.csv")
-        llamar_algoritmo(quicksort, array_10000, "QuickSort","nombres-10000-ordenados.csv")
-        pass
     else:
-        print("No eligio un argumento válido.")
+        print("No eligió un argumento válido.")
 
 if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,64 +1,83 @@
 import csv
 from faker import Faker
 
+# Crea listas de nombres a partir de archivos .csv
+
 def format(file, first, last):
     with open(file, 'r') as file:
         array=[]
+        # Crea un diccionario a partir del archivo csv ingresado
         csvreader = csv.DictReader(file)
         for row in csvreader:
             array.append(f"{row[first]} {row[last]}")
     return array    
 
+# Crea archivos .csv a partir de listas de nobmres
+# Revisar un poquito
 def write(data, file, first, last):
     with open(file, 'w', newline="") as file:
         writer = csv.writer(file)
-
         writer.writerow([first, last])
-
         for full_name in data:
             last, first = full_name.split()
             writer.writerow([last, first])
 
+# Este codigo es para crear listas de nombres aleatorias
+
+faker = Faker()
+
+# Donde empieza la iteración
+i = 10
+
+# Debe ser multiplo de 10
+n = 10000
+
+while i <= n:
+    lista = []
+    for _ in range(i):
+        lista.append(f"{faker.last_name()} {faker.first_name()}")
+    # Reutilizamos write
+    write(lista, f"nombres-{i}.csv", "Apellido", "Nombre")
+    i = i * 10
+
+# ------------------ Algoritmos de ordenamiento ------------------
 
 def bubble_sort(array):
-    n = len(array) # 2
 
-    # Como hay dos for loops anidados se ejecuta n * n
+    n = len(array) 
 
-    # Para los elementos i de la longitud del array
-    for i in range(n): # n * n * lo que hay dentro del for loop
+    for i in range(n):
+
         swapped = False
 
-        # Los ultimos elementos i ya estan en su lugar
-        for j in range(0, n-i-1): # n * lo que hay dentro de este for loop
-            # Se mueve a traves del array de 0 a n-i-1
-            # Cambia si el elemento encontrado es mayor
-            # que el siguiente elemento
-            if array[j][0] > array[j+1][0]: # 3 * n * n
-                array[j], array[j+1] = array[j+1], array[j] # 2
-                swapped = True # 1
+        for j in range(0, n-i-1):
+        
+
+            if array[j][0] > array[j+1][0]:
+
+                array[j], array[j+1] = array[j+1], array[j]
+
+                swapped = True
+        
         if (swapped == False):
             break
 
-
 def merge_sort(array):
-    # Recursivo
+    
     if len(array) > 1:
-        # Divide el array en dos mitades
+        
         left_array = array[:len(array)//2]
         right_array = array[len(array)//2:]
 
-        # Recursivamente
         merge_sort(left_array)
         merge_sort(right_array)
 
-        # Merge
-        # Comparamos los elementos
         i = 0 # compara el elemento de la izquierda del array izquierdo
         j = 0 # compara el elemento de la izquierda del array derecho
         k = 0 # el index del array mergeado
+
         while i < len(left_array) and j < len(right_array):
-            # Dividie entre apellido y nombre y compara los apellidos
+            
             if left_array[i].split()[0] < right_array[j].split()[0]:
                 array[k] = left_array[i]
                 i += 1
@@ -76,55 +95,3 @@ def merge_sort(array):
             array[k] = right_array[j]
             j += 1
             k += 1
-
-def quicksort(array, left, right):
-    if left < right:
-        partition_pos = partition(array, left, right)
-        quicksort(array, left, partition_pos - 1)
-        quicksort(array, partition_pos + 1, right)
-
-def partition(array, left, right):
-    i = left
-    j = right - 1
-    pivot = array[right]
-
-    while i < j:
-        while i < right and array[i] < pivot:
-            i += 1
-        while j > left and array[j] >= pivot:
-            j -= 1
-        if i < j:
-            array[i], array[j] = array[j], array[i]
-    if array[i] > pivot:
-        array[i], array[right] = array[right], array[i]
-    
-    return i
-
-# Este codigo es para crear listas de nombres aleatorias
-
-# Reutilizamos write
-
-lista = []
-faker = Faker()
-for _ in range(10):
-    lista.append(f"{faker.last_name()} {faker.first_name()}")
-
-write(lista, "nombres-10.csv", "Apellido", "Nombre")
-
-lista = []
-for _ in range(100):
-    lista.append(f"{faker.last_name()} {faker.first_name()}")
-
-write(lista, "nombres-100.csv", "Apellido", "Nombre")
-
-lista = []
-for _ in range(1000):
-    lista.append(f"{faker.last_name()} {faker.first_name()}")
-
-write(lista, "nombres-1000.csv", "Apellido", "Nombre")
-
-lista = []
-for _ in range(10000):
-    lista.append(f"{faker.last_name()} {faker.first_name()}")
-
-write(lista, "nombres-10000.csv", "Apellido", "Nombre")
